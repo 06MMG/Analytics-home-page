ce36f104d4efa61240ee9b7a092e76e3e4f10aa4
06MMG <124175643+06MMG@users.noreply.github.com>
2024-03-02 02:09:52 -0800

    first commit home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from './components/navbar';
+import Hero from "./components/hero";
+import Anylytics from "./components/anylytics";
+import Newletter from "./components/newletter";
+import Card from "./components/card";
+import Footer from "./components/footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+ <Navbar />
+ <Hero />
+ <Anylytics />
+ <Newletter />
+ <Card />
+ <Footer />
     </div>
   );
 }
